e67d29136db031bcc219da9a17cbe5397f3bad6f
ogaoga <info@ogaoga.org>
2012-12-12 00:01:44 +0900

    modified.

--- examples/parallax/script.js
+++ examples/parallax/script.js
@@ -1,5 +1,10 @@
 $(function(){
   var myScroll;
+  var contentHeight;
+  var contentWidth;
+  var points = [1000, 1300, 1556, 2000];
+  var bows;
+  var delay = 100;
 
   // on load
   $(window).bind('load', function(e){
@@ -7,42 +12,51 @@ $(function(){
   	myScroll = new iScroll('wrapper',
                            {bounce: false});
     // set rainbow height
-    var contentHeight = $('#wrapper').height();
-    $('.rainbow').css('height', contentHeight/7+1+'px');
+    contentHeight = $('#wrapper').height();
+    contentWidth = $('#wrapper').width();
+    bows = $('.rainbow');
+    bows
+      .css('height', contentHeight/7+1+'px')
+      .css('-webkit-transform', 'translate3d('+contentWidth+'px, 0, 0)');
   });
 
   $(document)
     .bind('touchmove', function (e) { e.preventDefault(); })
     .bind('iscroll', function(e, pos){
       console.log(pos);
-      if ( 0 <= pos.y && pos.y < 1000 ) {
+      if ( 0 <= pos.y && pos.y < points[0] ) {
         // set background color
         $('#wrapper').css('background', 'white');
         // slide in from right
-        var bows = $('.rainbow');
         for ( var i = bows.length-1 ; i >= 0 ; i-- ) {
-          var x = (320-pos.y+100*i);
-          x = (x >= 0) ? x : 0;
+          var x = (contentWidth - pos.y + delay * i);
+          x = (x > 0) ? x : 0;
           $(bows[i])
             .css('-webkit-transform',
                  'translate3d(' + x + 'px, ' + pos.y + 'px, 0)');
         }
       }
-      else if ( 1000 <= pos.y && pos.y < 1300 ) {
+      else if ( points[0] <= pos.y && pos.y < points[1] ) {
         // set background color
         $('#wrapper').css('background', 'black');
         // move up 
-        $('.rainbow')
-          .css('-webkit-transform',
-               'translate3d(0px, ' + (2000 - pos.y) + 'px, 0)');
+        bows.css('-webkit-transform',
+                 'translate3d(0px, ' + (points[0] + points[0] - pos.y) + 'px, 0)');
       }
-      else if ( 1300 <= pos.y && pos.y < 1556 ) {
-        var color = parseInt(pos.y - 1300 +1);
-        var colorStr = 'rgb('+color+', '+color+', '+color+')';
-        $('#wrapper').css('background-color', colorStr);
+      else if ( points[1] <= pos.y && pos.y < points[2] ) {
+        // change background color
+        var color = parseInt(pos.y - points[1] +1);
+        $('#wrapper').css('background-color',
+                          'rgb('+color+', '+color+', '+color+')');
       }
-      else if ( 1556 <= pos.y ) {
+      else if ( points[2] <= pos.y && pos.y < points[3] ) {
+        // show signature
         $('#wrapper').css('background-color', 'white');
+        if ( pos.y - points[2] < contentHeight/2 ) {
+          $('.signature')
+            .css('-webkit-transform',
+                 'translate3d(0, ' + (points[2] - pos.y) + 'px, 0)');
+        }
       }
       else {
       }

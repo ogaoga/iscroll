a2975848af5c0673095c9b56633b75c7920ab4a8
ogaoga <info@ogaoga.org>
2012-12-11 21:53:08 +0900

    Fade.

--- examples/parallax/script.js
+++ examples/parallax/script.js
@@ -1,14 +1,14 @@
 $(function(){
   var myScroll;
-  // ready
-  $(document).ready(function(){
-  	myScroll = new iScroll('wrapper',
-                           {bounce: false});
-  });
+
   // on load
   $(window).bind('load', function(e){
-    // refresh iScroll
-    myScroll.refresh();
+    // iScroll
+  	myScroll = new iScroll('wrapper',
+                           {bounce: false});
+    // set rainbow height
+    var contentHeight = $('#wrapper').height();
+    $('.rainbow').css('height', contentHeight/7+1+'px');
   });
 
   $(document)
@@ -16,6 +16,9 @@ $(function(){
     .bind('iscroll', function(e, pos){
       console.log(pos);
       if ( 0 <= pos.y && pos.y < 1000 ) {
+        // set background color
+        $('#wrapper').css('background', 'white');
+        // slide in from right
         var bows = $('.rainbow');
         for ( var i = bows.length-1 ; i >= 0 ; i-- ) {
           var x = (320-pos.y+100*i);
@@ -25,10 +28,21 @@ $(function(){
                  'translate3d(' + x + 'px, ' + pos.y + 'px, 0)');
         }
       }
-      else if ( 1000 <= pos.y && pos.y < 1200 ) {
+      else if ( 1000 <= pos.y && pos.y < 1300 ) {
+        // set background color
+        $('#wrapper').css('background', 'black');
+        // move up 
         $('.rainbow')
           .css('-webkit-transform',
                'translate3d(0px, ' + (2000 - pos.y) + 'px, 0)');
+      }
+      else if ( 1300 <= pos.y && pos.y < 1556 ) {
+        var color = parseInt(pos.y - 1300 +1);
+        var colorStr = 'rgb('+color+', '+color+', '+color+')';
+        $('#wrapper').css('background-color', colorStr);
+      }
+      else if ( 1556 <= pos.y ) {
+        $('#wrapper').css('background-color', 'white');
       }
       else {
       }
